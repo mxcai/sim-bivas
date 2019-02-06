3dfb5de65fc624ce9b9aa7011529ed486de078e7
mxcai <32085863+mxcai@users.noreply.github.com>
2019-02-06 10:22:30 +0800

    clean codes

--- B2.R
+++ B2.R
@@ -1,10 +1,8 @@
 # method to be compared: bivas, BSGS-SS, BGL-SS
 # comparison of FDR, power and auc and error; sparsity 0.05:0.8, 0.1:0.4, 0.2:0.2, 0.4:0.1, 0.8:0.05
 library(pROC)
-# library(grpreg)
 library(mvtnorm)
 library(bivas)
-library(IGESS)
 library(MBSGS)
 # set.seed(10)
 
@@ -62,40 +60,16 @@ for(i in 1:nrow(sparsity)) {
       y0 <- mu+X%*%beta_true
       se2_true <- var(y0) / snr
       y <- y0 + rnorm(n,0,sqrt(se2_true))
-      # y <- y-mean(y)
 
-      # out_varbvs <- varbvs(X=X,y=y,Z=NULL,verbose = F)
-      # out_igess  <- iGess(X,y,opts = list(max_iter=600, display_gap = 100))
       time_bivas <- system.time(out_bivas  <- bivas(y,X,group=g,coreNum = useCore,verbose = F))[3]
       time_BSGS <- system.time(out_BSGS   <- BSGSSS(y,X,group_size=l,niter=500,burnin=100,num_update = 20,niter.update = 20))[3]
-      # time_BGL <- system.time(out_BGL    <- BGLSS(y,X,group_size=l,niter=500,burnin=100,num_update = 20,niter.update = 20))[3]
 
-      # FDR_varbvs <- 1 - sum((fdr2FDR(1-with(out_varbvs,alpha%*%normalizelogweights(logw)))<0.1)+(gamma*eta==1)==2) / sum(fdr2FDR(1-with(out_varbvs,alpha%*%normalizelogweights(logw)))<0.1)
-      # FDR_bivas  <- 1 - sum(fdr(out_bivas,control="global")$FDR+(gamma*eta==1)==2) / sum(fdr(out_bivas,control="global")$FDR)
-      # FDR_igess  <- 1 - sum((fdr2FDR(1-out_igess$gammas)<0.1)+(gamma*eta==1)==2) / sum(fdr2FDR(1-out_igess$gammas)<0.1)
-      # FDR_BSGS  <- 1 - sum((out_BSGS$pos_median!=0) + (gamma*eta==1)==2) / sum((out_BSGS$pos_median!=0))
-      # FDR_BGL  <- 1 - sum((out_BGL$pos_median!=0) + (gamma*eta==1)==2) / sum((out_BGL$pos_median!=0))
 
-      # auc_varbvs <- auc(eta*gamma,as.vector(with(out_varbvs,alpha%*%normalizelogweights(logw))))
-      # auc_bivas  <- auc(eta*gamma,as.vector(getPos(out_bivas)$var_pos))
-      # auc_igess  <- auc(eta*gamma,as.vector(out_igess$gammas))
-      # auc_BSGS  <-
-
-      # power_varbvs <- sum((fdr2FDR(1-with(out_varbvs,alpha%*%normalizelogweights(logw)))<0.1)+(gamma*eta==1)==2) / sum(gamma*eta==1)
-      # power_bivas  <- sum(fdr(out_bivas,control="global")$FDR+(gamma*eta==1)==2) / sum(gamma*eta==1)
-      # power_igess  <- sum((fdr2FDR(1-out_igess$gammas)<0.1)+(gamma*eta==1)==2) / sum(gamma*eta==1)
-      # power_BSGS  <- sum((out_BSGS$pos_median!=0)+(gamma*eta==1)==2) / sum(gamma*eta==1)
-      # power_BGL  <- sum((out_BGL$pos_median!=0)+(gamma*eta==1)==2) / sum(gamma*eta==1)
-
-      # error_varbvs <- mean((beta_true-with(out_varbvs,(alpha*mu)%*%normalizelogweights(logw)))^2)
       error_bivas  <- mean((beta_true-coef(out_bivas)$beta)^2)
-      # error_igess  <- mean((beta_true-out_igess$gammas*out_igess$mu)^2)
       error_BSGS   <- mean((beta_true-out_BSGS$pos_median)^2)
-      # error_BGL    <- mean((beta_true-out_BGL$pos_median)^2)
 
       out <- rbind(out,data.frame(time=time_bivas,error=error_bivas,method="BIVAS",sparsity=paste(sparsity$pi[i],":",sparsity$alpha[i],sep = ""),corr=paste(corr[t])))
       out <- rbind(out,data.frame(time=time_BSGS,error=error_BSGS,method="BSGS-SS",sparsity=paste(sparsity$pi[i],":",sparsity$alpha[i],sep = ""),corr=paste(corr[t])))
-      # out <- rbind(out,data.frame(FDR=FDR_BGL,time=time_BGL,power=power_BGL,error=error_BGL,method="BGL-SS",sparsity=paste(sparsity$pi[i],":",sparsity$alpha[i],sep = "")))
     }
   }
 }
